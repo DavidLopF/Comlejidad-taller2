61e0089405e4f474f5f7eec9437aabdad2b00b1c
gitForDavid <dlopezf@unbosque.edu.co>
2021-10-05 22:27:27 -0500

    kmp in model

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -23,26 +23,7 @@ public class Controller extends Component implements ActionListener {
 
     }
     
-    public ArrayList<Integer> KMP(String cadena1, String subcadena) {
-    	ArrayList<Integer> lugares = new ArrayList<Integer>();
-        int lugar = 0;
-        for(int a=0;a<cadena1.length();a++){
-          boolean comp = false;
-          for(int b=0;b<subcadena.length();b++){
-            if(cadena1.charAt(a+b)!=subcadena.charAt(b)){
-              b = a+subcadena.length();
-            }
-            else if(b==subcadena.length()-1){
-              comp = true;
-              lugar = a+1;
-              }
-            }
-            if(comp == true){
-            lugares.add(lugar);
-          }
-        }
-        return lugares;
-    }
+
     
     public void asignarListeners() {
         view.getPanelInicio().getChoose().addActionListener(this);
--- src/co/edu/unbosque/model/Algoritmos.java
+++ src/co/edu/unbosque/model/Algoritmos.java
@@ -1,10 +1,28 @@
 package co.edu.unbosque.model;
 
-import co.edu.unbosque.model.persistance.Archivo;
 
-import java.io.File;
+import java.util.ArrayList;
 
 public class Algoritmos {
 
-
+    public ArrayList<Integer> KMP(String cadena1, String subcadena) {
+        ArrayList<Integer> lugares = new ArrayList<Integer>();
+        int lugar = 0;
+        for(int a=0;a<cadena1.length();a++){
+            boolean comp = false;
+            for(int b=0;b<subcadena.length();b++){
+                if(cadena1.charAt(a+b)!=subcadena.charAt(b)){
+                    b = a+subcadena.length();
+                }
+                else if(b==subcadena.length()-1){
+                    comp = true;
+                    lugar = a+1;
+                }
+            }
+            if(comp == true){
+                lugares.add(lugar);
+            }
+        }
+        return lugares;
+    }
 }
